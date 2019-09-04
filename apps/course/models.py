660c407a7b3e6c661bf008ec992ce75f1f0606f3
Ryan <1255118460@qq.com>
2019-09-04 22:48:22 +0800

    add vcs

--- apps/course/models.py
+++ apps/course/models.py
@@ -9,7 +9,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u"课程名称")
     desc = models.CharField(max_length=300, verbose_name=u'课程描述')
     detail = models.TextField(verbose_name=u'课程详情')
-    degree = models.CharField(choices=(('dj', '低级'), ('zj', u'中级'), ('gj', u'高级')), max_length=2)
+    degree = models.CharField(choices=(('dj', '低级'), ('zj', u'中级'), ('gj', u'高级')), max_length=2, verbose_name='难度')
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长（分钟）")
     students = models.IntegerField(default=0, verbose_name=u"学习人数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
@@ -21,6 +21,9 @@ class Course(models.Model):
         verbose_name = '课程'
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u'课程', on_delete=models.CASCADE)
@@ -30,6 +33,9 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = '章节'
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Video(models.Model):
